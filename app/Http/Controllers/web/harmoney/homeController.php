23e4e8832bcef884ba7f2a0dae378d69c931a7b4
sgr2010 <sgrdtk@gmail.com>
2016-09-26 20:15:23 +0900

    new

---
+++ app/Http/Controllers/web/harmoney/homeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\web\harmoney;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class homeController extends Controller
+{
+    //
+
+	public function index()
+	{
+		return view('harmoney/home.twig');
+	}
+
+	public function contactus()
+	{
+		return view('harmoney/contactus.twig');	
+	}
+
+	public function courses()
+	{
+		return view('harmoney/courses.twig');	
+	}
+
+
+}
